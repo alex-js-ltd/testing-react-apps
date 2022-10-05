e68f45db3072a82bc8b4495318421f4ac7b398aa
Alex Gilbert <alex.js.ltd@gmail.com>
2022-10-05 13:32:25 +0200

    update

---
+++ src/comps/login.tsx
@@ -0,0 +1,42 @@
+// http://localhost:3000/login
+// this renders a login UI and calls the onSubmit handler with the username
+// and password when the user submits the form.
+
+import React, { FC, FormEvent } from 'react'
+
+const Login: React.FC<{ onSubmit: Function }> = ({ onSubmit }) => {
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+
+        const form = event.currentTarget
+
+        const formElements = form.elements as typeof form.elements & {
+            username: HTMLInputElement
+            password: HTMLInputElement
+        }
+
+        const { username, password } = formElements
+
+        onSubmit({
+            username: username.value,
+            password: password.value,
+        })
+    }
+    return (
+        <form onSubmit={handleSubmit}>
+            <div>
+                <label htmlFor="username-field">Username</label>
+                <input id="username-field" name="username" type="text" />
+            </div>
+            <div>
+                <label htmlFor="password-field">Password</label>
+                <input id="password-field" name="password" type="password" />
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    )
+}
+
+export default Login
