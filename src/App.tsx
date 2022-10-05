39dbe8603647fbf72ee17dd4898ee09e99ba7c1f
Alex Gilbert <alex.js.ltd@gmail.com>
2022-10-05 14:34:17 +0200

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from 'react'
+import Login from 'comps/login'
 
 const App: FC = () => {
-    return <div>App</div>
+    return (
+        <div>
+            <Login onSubmit={() => console.log('onSubmit')} />
+        </div>
+    )
 }
 
 export default App
