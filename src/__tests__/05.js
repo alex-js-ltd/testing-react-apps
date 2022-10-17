8ba37e56efd7f40d33a7d81c86e337b2f41805ad
Alex Gilbert <alex.js.ltd@gmail.com>
2022-10-17 14:50:53 +0200

    update

--- src/__tests__/05.js
+++ src/__tests__/05.js
@@ -8,6 +8,7 @@ import {
 } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { build, fake } from '@jackfranklin/test-data-bot'
+import { rest } from 'msw'
 import { setupServer } from 'msw/node'
 import { handlers } from 'test/server-handlers'
 import Login from 'comps/login-submission'
@@ -51,4 +52,25 @@ test('omitting the password results in an error', async () => {
     expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(
         `"password required"`
     )
+})
+
+test('unknown server error displays the error message', async () => {
+    const testErrorMessage = 'Oh no, something bad happened'
+    server.use(
+        rest.post(
+            `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_API_KEY}`,
+            async (req, res, ctx) => {
+                return res(
+                    ctx.status(500),
+                    ctx.json({ error: { message: testErrorMessage } })
+                )
+            }
+        )
+    )
+    render(<Login />)
+    await userEvent.click(screen.getByRole('button', { name: /submit/i }))
+
+    await waitForElementToBeRemoved(() => screen.getByLabelText(/loading/i))
+
+    expect(screen.getByRole('alert')).toHaveTextContent(testErrorMessage)
 })
